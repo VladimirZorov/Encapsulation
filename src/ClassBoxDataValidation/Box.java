3091dd3a01b0f3a876d5aca0ea5b7e46ef573cb8
Vladimir Zorov <vl.zorov@gmail.com>
2023-01-13 19:09:38 +0200

    add

--- src/ClassBoxDataValidation/Box.java
+++ src/ClassBoxDataValidation/Box.java
@@ -7,23 +7,29 @@ public class Box {
     private double height;
 
     public Box(double length, double width, double height) {
-        this.length = length;
-        this.width = width;
-        this.height = height;
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
     }
 
     public void setLength(double length) {
-        checkForNegative(length, "Length");
+        if (length <=0) {
+            throw new IllegalArgumentException("Length cannot be zero or negative.");
+        }
         this.length = length;
     }
 
     public void setWidth(double width) {
-        checkForNegative(width, "Width");
+        if (width <=0) {
+            throw new IllegalArgumentException("Width cannot be zero or negative.");
+        }
         this.width = width;
     }
 
     public void setHeight(double height) {
-        checkForNegative(height, "Height");
+        if (height <=0) {
+            throw new IllegalArgumentException("Height cannot be zero or negative.");
+        }
         this.height = height;
     }
 
