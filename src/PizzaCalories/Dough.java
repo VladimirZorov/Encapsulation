3fd2312701349918f895f9fe7b0a5433134d1597
Vladimir Zorov <vl.zorov@gmail.com>
2023-01-14 14:49:59 +0200

    add

---
+++ src/PizzaCalories/Dough.java
@@ -0,0 +1,31 @@
+package PizzaCalories;
+
+public class Dough {
+
+    private String flourType;
+    private String bakingTechnique;
+    private double weight;
+
+    public Dough(String flourType, String bakingTechnique, double weight) {
+        this.setFlourType(flourType);
+        this.bakingTechnique = bakingTechnique;
+        this.setWeight(weight);
+    }
+
+
+    public void setWeight(double weight) {
+        if (weight < 0 || weight> 200) {
+            throw new IllegalArgumentException("Dough weight should be in the range [1..200].");
+        }
+        this.weight = weight;
+    }
+
+    public void setFlourType(String flourType) {
+        if (!flourType.equals("white") || !flourType.equals("wholegrain")) {
+            throw new IllegalArgumentException("Invalid type of dough.");
+        }
+        this.flourType = flourType;
+    }
+
+
+}
---
+++ src/PizzaCalories/Pizza.java
@@ -0,0 +1,12 @@
+package PizzaCalories;
+
+import java.util.List;
+
+public class Pizza {
+
+    private String name;
+    private Dough dought;
+    private List<Topping> toppings;
+
+
+}
